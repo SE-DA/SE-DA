509c0d46ba3a6b4d18d5bda6498ba56dcda24c8c
Wojciech Matusiak <renard227@gmail.com>
2020-01-29 20:30:51 +0100

    test21 - ukrycie lotów, których ilość na strefie Stock jest niewiększa niż 0

--- seda_1/models/stock_production_lot.py
+++ seda_1/models/stock_production_lot.py
@@ -8,6 +8,26 @@ import datetime
 class ProductionLot(models.Model):
     _inherit = "stock.production.lot"
 
+    @api.depends('quant_ids')
+    def _get_pq(self):
+        locs = self.env['stock.location'].search([('parent_path', 'ilike', '1/7/8/%')])
+        locs_ids = [x.id for x in locs]
+        for lot in self:
+            quants = self.env['stock.quant'].search([
+                ('quantity', '>', 0),
+                ('lot_id', '=', lot.id),
+                # ('reserved_quantity', '=', 0),
+                ('product_id', '=', lot.product_id.id),
+                ('location_id', 'in', locs_ids),
+            ], order='quantity desc', limit=1)
+
+            if quants and quants.quantity>quants.reserved_quantity:
+                lot.positive_q = True
+            else:
+                lot.positive_q = False
+        return False
+
+    positive_q = fields.Boolean('PQ', compute='_get_pq',store=True)
     lead_id = fields.Many2one('crm.lead','Komisja')
     owner_id = fields.Many2one('res.partner', 'Właściciel')
 
